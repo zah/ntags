fc3f43e4a846c7d06490770ad639d9b54bb29990
Zahary Karadjov <zahary@gmail.com>
2025-01-30 11:34:42 +0200

    Add a basic nix/direnv dev environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A development environment for a Nim project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nim
+          ];
+
+          shellHook = ''
+            echo "Welcome to your Nim development environment!"
+          '';
+        };
+      }
+    );
+}
